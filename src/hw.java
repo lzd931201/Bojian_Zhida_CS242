e7473cab49dfe49e5c5486b48c38336943f195f5
zhida liu <lzd93@qq.com>
2016-10-23 21:17:58 -0700

    parse one brand
    parse one brand’s list and get the nextpage link and the content of the
    product

--- src/hw.java
+++ src/hw.java
@@ -10,21 +10,31 @@ import org.jsoup.select.Elements;
 public class hw {
 
 	public static void main(String[] args)  throws IOException {
-//		Connection.Response html = Jsoup.connect("https://www.amazon.com/Computers-Tablets/b/ref=nav_shopall_basedevices?ie=UTF8&node=13896617011").execute();
+//		String URLs="https://www.amazon.com/s/ref=s9_acss_bw_cg_lgopc_3c1?node=13896617011&brand=Lenovo&lo=computers&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=unified-hybrid-12&pf_rd_r=ZAP8V9G9C9P58M9G4V44&pf_rd_t=101&pf_rd_p=2598218402&pf_rd_i=13896617011";
+////		String URL_com_and_tab = "https://www.amazon.com/Computers-Tablets/b/ref=nav_shopall_basedevices?ie=UTF8&node=13896617011";
+//		Connection.Response html = Jsoup.connect(URLs).execute();
 //		//Document doc = Jsoup.connect("http://www.amazon.com/").get();
 //		 System.out.print(html.body());
-//		 try(  PrintWriter out = new PrintWriter( "amazon_computer&tablets.html" )  ){
+//		 try(  PrintWriter out = new PrintWriter( "TEST.html" )  ){
 //			    out.println( html.body() );
 //			}
 
-		// TODO Auto-generated method stub
-		File input = new File("amazon_computer&tablets.html");
-		Document doc = Jsoup.parse(input, "UTF-8");
+		//parse an exist file
+		File input = new File("TEST.html");
+		Document doc = Jsoup.parse(input, "UTF-8");		
 		Elements links =doc.getElementsByTag("a");
+		String last=null;
 		for (Element link : links) {
+			
 		  String linkHref = link.attr("href");
-		  System.out.println(linkHref);
+		  if(linkHref.contains("www.amazon.com/")){
+			  if(!linkHref.equals(last)){	
+				  last=linkHref;
+				  System.out.println(linkHref);
+			  }		//reduce duplicate
+		  }
 		}
+		
 	}
 
 }
