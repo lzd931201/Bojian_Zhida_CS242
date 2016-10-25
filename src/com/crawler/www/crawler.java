d12c2aa3277aaab49e88cad434cc74d1af335c6c
zhida liu <lzd93@qq.com>
2016-10-25 12:09:43 -0700

    crawler

---
+++ src/com/crawler/www/crawler.java
@@ -0,0 +1,118 @@
+package com.crawler.www;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.Queue;;
+
+public class crawler {
+	
+	static Queue<String> URLS = new LinkedList<String>();	//contain the URLs for crawl
+	
+	
+	
+	public static void Crawl(String fileName,Integer fileCount)throws IOException{
+		BufferedReader buff = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		try {
+		    String URL;
+		    while ((URL = buff.readLine()) != null) {
+		       AddQueue(URL);
+		    }	//add seeds into the queue
+		} finally {
+		    buff.close();
+		}
+		ParsePage(fileCount);	//start parse
+	}	// crawl using the seeds in a file and get a certain amount of file
+	
+	private static void AddQueue(String URL){
+		URLS.add(URL);
+	}	//add a single URL in to the Queue
+	
+	private static String GetNextPage(){
+		return URLS.poll();
+	}	//get the head of the Queue
+	
+	private static Integer StorageCount =0;	// indicate how many files have been stored
+	
+	 private static void ParsePage(Integer fileCount)throws IOException{
+		while(!URLS.isEmpty()&&StorageCount<fileCount){
+			String URL = GetNextPage();
+			Parse(URL,parseType.NEXTLINK);
+			Parse(URL,parseType.PRODUCT);
+		}
+	}	//put the nextpage of the current page in to the queue and then save products
+	
+	public enum parseType{
+		PRODUCT("s-item-container"),
+		NEXTLINK("pagnNext");
+		private String classID;
+		private parseType(String id){
+			this.classID=id;
+		}
+		public String getclassID(){
+			return this.classID;
+		}
+	}
+	
+	private static String Domain="http://www.amazon.com";
+	
+	private static void Parse(String URL, parseType t)throws IOException{
+		Document doc = Jsoup.connect(URL).timeout(30000)
+				.userAgent(
+						"Mozilla/5.0 (Windows NT 6.1; WOW64)")
+				.get();
+		Elements buttons = doc.getElementsByClass(t.getclassID());
+		String lastLink = null;
+		switch(t){
+		case PRODUCT:
+			for(Element button:buttons){
+				Elements links = button.getElementsByTag("a");
+				for(Element link:links){
+					if(!link.attr("href").equals(lastLink)){
+						SaveProduct(link.attr("href"));
+						lastLink = link.attr("href");
+					}
+				}
+			}
+		case NEXTLINK:
+			for(Element button :buttons){
+				String link = button.attr("href");
+				if(link==null){
+					
+				}
+				else if(link.startsWith("www")){
+					link = "http://"+link;
+					AddQueue(link);
+					
+				}
+				else if(link.startsWith("/")){
+					link = Domain+link;
+					AddQueue(link);
+				}
+			}
+		}
+	}
+	
+	
+	
+	private static void SaveProduct(String URL)throws IOException{
+		Document doc = Jsoup.connect(URL).timeout(30000)
+				.userAgent(
+						"Mozilla/5.0 (Windows NT 6.1; WOW64)")
+				.get();
+		try (PrintWriter out = new PrintWriter(StorageCount.toString()+".html")) {
+			out.println(doc.body());
+		}
+		StorageCount++;
+	}
+}
---
+++ src/com/crawler/www/main.java
@@ -0,0 +1,9 @@
+package com.crawler.www;
+
+import java.io.IOException;
+
+public class main {
+	public static void main(String[] args)  throws IOException {
+		crawler.Crawl("Seeds.txt",12500);
+	}
+}
